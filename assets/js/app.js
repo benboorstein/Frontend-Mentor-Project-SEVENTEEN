84070b6b25266c863a27590cda4f65e05daf1174
Ben <benboorstein@gmail.com>
2022-03-10 14:55:27 -0500

    initial commit for Frontend Mentor project seventeen

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+const App = {
+    data() {
+        return {
+
+        }
+    }
+}
+
+Vue.createApp(App).mount('#app')
+
+
+
+
+
+
+
+// QQQ: For "nine..." version and this version, the console error 6 times (I'm sure because svg is there 6 times): Error: <svg> attribute height: Expected length, "auto".
